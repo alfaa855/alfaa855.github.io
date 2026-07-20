79623cdd9beef01f7f95472cab104f7e0643ff8c
alfaa855 <alfaaaa290710@gmail.com>
2026-07-20 23:21:52 +0700

    Improve product search and detail interaction
    Refactor product search and detail display logic. Update event listeners and overlay handling.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-    // untuk search engine di website nya 
+document.addEventListener('DOMContentLoaded', function () {
+    // untuk search exact di website nya //
 const searchInput = document.getElementById('searchInput');
 const products = document.querySelectorAll('.product');
 
@@ -23,9 +23,8 @@ searchInput.addEventListener('input',function(){
 
 //urutkan nama//
 const productContainer = document.querySelector('.products')
-const listProduct = document.querySelectorAll('.product')
 
-const arrayProduct = Array.from(listProduct)
+const arrayProduct = Array.from(products)
 arrayProduct.sort(function(productA, productB) {
     const nama1 = productA.querySelector('.productname').textContent.toLowerCase();
     const nama2 = productB.querySelector('.productname').textContent.toLowerCase();
@@ -35,36 +34,72 @@ arrayProduct.forEach(function(item){
     productContainer.appendChild(item)
 });
 
+//urutkan stok//
+
 
 //product detail//
 const readBox = document.querySelector(".productDetail");
+const PDoverlay = document.querySelector('.PD-overlay');
+const allPDRead = document.querySelectorAll('.PD-ProductRead');
+const PDsupplierRead = document.querySelector('.PD-SupplierRead');
+const PDorderInput = document.querySelector('.PD-ProductOrder');
+//body element//
+const body = document.querySelector('body');
+//save and cancel button//
 const PDsaveandcancel = document.querySelector('.PD-product-read-btn');
 const PIsaveandcancel = document.querySelector('.PI-product-input-btn');
-const overlay = document.querySelector('.PDPI-overlay');
-const body = document.querySelector('body')
 const PIcancel = document.querySelector('.PIcancel');
 const PDcancel = document.querySelector('.PDcancel');
-const allPDRead = document.querySelectorAll('.PD-ProductRead');
-const PDsupplierRead = document.querySelectorAll('.PD-SupplierRead');
-const PDorderInput = document.querySelectorAll('.PD-ProductOrder');
 //Product Input//
-
 const inputBox = document.querySelector('.productInput');
 const allPIInput = document.querySelectorAll('.PI-ProductInput');
 const PIsupplierInput = document.querySelectorAll('.PI-SupplierInput');
+const PIoverlay = document.querySelector('.PI-overlay');
 //btn group//
 const GrupBtn = document.querySelector('.btnGroup');
 const GroupBTND = document.querySelector('.btnGroupDisplay');
 const inputbtn = document.querySelector('.plus-btn');
 const checkbtn  = document.querySelector('.checkBtn');
 const Xbtn = document.querySelector('.in-cancelHam');
+//in check button//
+const copyBox = document.querySelector('.copyBox');
+const backCopy = document.querySelector('.in-backbtn');
+const grupMenu= document.querySelector('.in-CBgrupBtn');
+const aftergrupMenu= document.querySelector('.after-in-CBgrupBtn');
+const CBmenubox = document.querySelector('.CBmenuBox');
+const CBhistory = document.querySelector('.CBhistory');
+const CBsaveData = document.querySelector('.CBsaveData');
+const CBcancelData = document.querySelector('.CBcancelData');
 
+function resetHamBtn(){
+    GrupBtn.style.display ="flex";
+    GroupBTND.style.display ="none";
+    inputbtn.style.display ="none";
+    checkbtn.style.display ="none";
+    Xbtn.style.display ="none";
+}
+function onHamBtn(){
+    GrupBtn.style.display ="none";
+    GroupBTND.style.display ="flex";
+    inputbtn.style.display ="flex";
+    checkbtn.style.display ='flex';
+    Xbtn.style.display ="flex";
+}
+
+function OnCBmenuBox(){
+    CBmenubox.style.display= "flex";
+}
+function OffCBmenuBox(){
+    CBmenubox.style.display= "none";
+}
 products.forEach(function(product){
     product.addEventListener("click", function(){
-            readBox.style.display = "flex"
-            PDsaveandcancel.style.display ="flex"
-            overlay.style.display = "block"
-            body.style.overflow= "hidden"
+            readBox.style.display = "flex";
+            PDsaveandcancel.style.display ="flex";
+            PDoverlay.style.display = "block";
+            body.style.overflow= "hidden";
+            inputBox.style.display ="none";
+            resetHamBtn();
          allPDRead.forEach(function(read) {
          read.readOnly= true;
          });
@@ -78,7 +113,7 @@ products.forEach(function(product){
     PDcancel.addEventListener("click", function(){
         readBox.style.display= "none";
         PDsaveandcancel.style.display="none";
-        overlay.style.display= "none";
+        PDoverlay.style.display= "none";
         body.style.overflow = "auto";
         allPDRead.forEach(function(read){
         read.readOnly = true;
@@ -101,7 +136,7 @@ products.forEach(function(product){
     PIcancel.addEventListener("click", function(){
         inputBox.style.display= "none";
         PIsaveandcancel.style.display="none";
-        overlay.style.display= "none";
+        PIoverlay.style.display= "none";
         body.style.overflow = "auto";
         allPIInput.forEach(function(read){
         read.readOnly = false;
@@ -119,43 +154,51 @@ products.forEach(function(product){
     //save btn//
     //sampai sini//
     //overlay click//
-    overlay.addEventListener("click", function(){
-        inputBox.style.display ="none"
-        PDsaveandcancel.style.display ="none"
-        PIsaveandcancel.style.display ="none"
-        overlay.style.display ="none"
-        GrupBtn.style.display ="flex"
-        GroupBTND.style.display ="none"
-        Xbtn.style.display ="none"
-        inputbtn.style.display ="none"
-        checkbtn.style.display ="none"
-        body.style.overflow ="auto"
-
+    PIoverlay.addEventListener("click", function(){
+        inputBox.style.display ="none";
+        PIsaveandcancel.style.display ="none";
+        PIoverlay.style.display = "none";
+        body.style.overflow ="auto";
     });
     //grup btn//
     GrupBtn.addEventListener("click", function(){
-        GroupBTND.style.display = "flex"
-        inputbtn.style.display = "flex"
-        checkbtn.style.display ="flex"
-        GrupBtn.style.display ="none"
-        Xbtn.style.display = "flex"
+        onHamBtn();
     });
     //cancel btn grup//
     Xbtn.addEventListener("click", function(){
-        GroupBTND.style.display = "none"
-        inputbtn.style.display ="none"
-        checkbtn.style.display ="none"
-        GrupBtn.style.display = "flex"
-        Xbtn.style.display ="none"
+        resetHamBtn();
     });
     //Input button//
 inputbtn.onclick = () =>{
     inputBox.style.display = "flex";
-    overlay.style.display = "block";
+    PIoverlay.style.display = "block";
     PIsaveandcancel.style.display = "flex";
     body.style.overflow = "hidden";
+    resetHamBtn();
 };
 //sampai sini//
+//check button//
+checkbtn.onclick =()=> {
+    resetHamBtn();
+    copyBox.style.display = "flex";
+    body.style.overflow="hidden";
+}
+grupMenu.onclick = ()=>{
+    OnCBmenuBox();
+    grupMenu.style.display= "none";
+    aftergrupMenu.style.display = "flex";
+}
+aftergrupMenu.onclick = ()=>{
+    OffCBmenuBox();
+    grupMenu.style.display= "flex";
+    aftergrupMenu.style.display = "none";
+}
+//back btn//
+backCopy.onclick =() =>{
+    copyBox.style.display = "none";
+    body.style.overflow= "auto";
+    OffCBmenuBox();
+}
 //anti zoom di hp//
 document.addEventListener('touchmove',function (event) {
     if (event.touches.length > 1 ){
@@ -179,4 +222,5 @@ document.addEventListener('touchend', function(zoom){
         };
     }, {passive: false});
 
+});//DOM content//
 
